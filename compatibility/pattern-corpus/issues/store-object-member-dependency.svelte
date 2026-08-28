b38a9e2f8292a6c644b98f30bc57c6fe7f322b9e
baseballyama <xydybaseball@gmail.com>
2026-08-29 02:21:59 +0900

    Keep store object dependencies distinct

---
+++ compatibility/pattern-corpus/issues/store-object-member-dependency.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Widget from './Widget.svelte';
+	import { settings } from './stores.js';
+
+	$: current = $settings.current;
+</script>
+
+<Widget value={current} onSave={settings.set} />
